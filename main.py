0f76235d92f76e6e277780896b247a2153a4642e
Bogdan Buduroiu <bbuduroiu@gmail.com>
2018-02-24 21:57:28 +0000

    FInished init anomaly detector

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 from anogan.anogan import AnoGAN
 
 if __name__ == '__main__':
     model = AnoGAN()
+    training_result = model.train(epochs=300, print_interval=10)
 
-    eval = model.train(epochs=300, print_interval=10)
+    model.anomaly_detector()
+    errors = model.train_anomaly_detector(epochs=3, print_interval=1)
 
-    print(eval)
-    print(np.mean(eval), np.std(eval))
+    plt.scatter(errors[0,:,0], errors[0,:,1])
+    plt.show()
