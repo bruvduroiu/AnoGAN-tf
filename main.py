5aa456126b072b97dc22b94081a33d57fe8d2088
Bogdan Buduroiu <bbuduroiu@gmail.com>
2018-02-27 11:10:33 +0000

    Added click options

--- main.py
+++ main.py
@@ -7,10 +7,12 @@ from anogan.anogan import AnoGAN
 
 @click.command()
 @click.option('--display/--no-display', default=False)
-def main(display):
+@click.option('--epochs', '-e', default=300, help='Num. epochs to train for.')
+@click.option('--print-interval', '-p', default=10, help='Print output every x epochs.')
+def main(display, epochs, print_interval):
     tf.reset_default_graph()
     model = AnoGAN()
-    training_result = model.train(epochs=300, print_interval=10)
+    training_result = model.train(epochs=epochs, print_interval=print_interval)
 
     model.anomaly_detector()
     generated, test_data = model.train_anomaly_detector(epochs=3, print_interval=1)
